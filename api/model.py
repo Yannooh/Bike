bcdb5ae4fb1bbc7b522cf95f8402e180ccee7695
antoine-cr <92589163+antoine-cr@users.noreply.github.com>
2021-11-26 16:42:38 +0100

    variables en plus

--- api/model.py
+++ api/model.py
@@ -1,21 +1,21 @@
 from pydantic import BaseModel
 from typing import List
-
 from enum import Enum
 
 
-class WeathersitEnum(str, Enum)
+class WeathersitEnum(str, Enum):
     clear = 'clear'
     snowy = 'snowy'
     rainy = 'rainy'
     cloudy = 'cloudy'
-    ...
 
-
-
-class BikeHourData(BaseModel)
+class BikeHourData(BaseModel):
     hr : int
     weathersit : WeathersitEnum
+    hum : float
+    windspeed : float
+    temp : float
+    atemp : float
 
 
 BikeData = List[BikeHourData]
