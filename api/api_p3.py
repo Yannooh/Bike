c4d65d5047c10f933766ee9952b1d1a1fb5c4dea
YannickNF <yannicknfene@gmail.com>
2021-11-28 14:20:06 +0100

    Evol_files

--- api/api_p3.py
+++ api/api_p3.py
@@ -4,12 +4,13 @@ import numpy as np
 import pandas as pd
 import joblib
 from model import pylance
+from model import BikeData
 
 
-data = pd.read_csv('https://assets-datascientest.s3-eu-west-1.amazonaws.com/de/total/bike.csv')
+# data = pd.read_csv('https://assets-datascientest.s3-eu-west-1.amazonaws.com/de/total/bike.csv')
 
-model_rf = joblib.load("rf_model.joblib")
-model_gb = joblib.load("gb_model.joblib")
+# model_rf = joblib.load("rf_model.joblib")
+# model_gb = joblib.load("gb_model.joblib")
 
 #Declaration de l'API
 
@@ -22,31 +23,42 @@ def get_status():
     return {'1'}
     abort(404)
 
-@api.get('/bike_data')
-def get_bike_data():
-    return FileResponse('./bike_by_day.json')
-    abort(404)
+# @api.get('/bike_data')
+# def get_bike_data():
+#     return FileResponse('./bike_by_day.json')
+#     abort(404)
 
 
-@api.get('/bike_data_day')
-def get_bike_data_day():
-    return df
-    abort(404)
+# @api.get('/bike_data_day')
+# def get_bike_data_day():
+#     return df
+#     abort(404)
 
-@api.get('/predict_random_forest')
-def get_bike_data():
-    return np.sqrt(mean_squared_error(y_train, rf.predict(X_train)))
-        #print("\nRMSE_test:",np.sqrt(mean_squared_error(y_test, gb.predict(X_test))))
-    abort(404)
+# @api.get('/predict_random_forest')
+# def get_bike_data():
+#     return np.sqrt(mean_squared_error(y_train, rf.predict(X_train)))
+#         #print("\nRMSE_test:",np.sqrt(mean_squared_error(y_test, gb.predict(X_test))))
+#     abort(404)
 
-@api.get('/predict_regression_lineaire')
-def get_bike_data():
-    return mean_squared_error(y_train,y_pred_train,squared=False)
-        #print("\nRMSE_test:",np.sqrt(mean_squared_error(y_test, gb.predict(X_test))))
-    abort(404)
+# @api.get('/predict_regression_lineaire')
+# def get_bike_data():
+#     return mean_squared_error(y_train,y_pred_train,squared=False)
+#         #print("\nRMSE_test:",np.sqrt(mean_squared_error(y_test, gb.predict(X_test))))
+#     abort(404)
 
-@api.get('/predict_gradient_boost')
-def get_bike_data():
-    return np.sqrt(mean_squared_error(y_train, gb.predict(X_train)))
-        #print("\nRMSE_test:",np.sqrt(mean_squared_error(y_test, gb.predict(X_test))))
-    abort(404)
+# @api.get('/predict_gradient_boost')
+# def get_bike_data():
+#     return np.sqrt(mean_squared_error(y_train, gb.predict(X_train)))
+#         #print("\nRMSE_test:",np.sqrt(mean_squared_error(y_test, gb.predict(X_test))))
+#     abort(404)
+
+# @api.post('/rf_model')
+# def get_bike_data(data:Bikedata):
+#     return data
+
+#     transformed_data = transform_data(data)
+#     return data
+
+# @api.post('/gb_model')
+# def get_bike_data(data:Bikedata):
+#     return data
