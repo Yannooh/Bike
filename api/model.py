0e6d3ce7dd3c8842c28f0c23c25dd8fa61917f73
Yannooh <93126185+Yannooh@users.noreply.github.com>
2021-11-28 13:58:41 +0100

    Update model.py

--- api/model.py
+++ api/model.py
@@ -1,15 +1,18 @@
+# Imporatation des librairies
 from pydantic import BaseModel
 from typing import List
 from enum import Enum
 
-
+# Définition de l'énumération class WeathersitEnum
 class WeathersitEnum(str, Enum):
+    ''' cette class permet de définir des choix au nombre de 4 au total '''
     clear = 'clear'
     snowy = 'snowy'
     rainy = 'rainy'
     cloudy = 'cloudy'
 
 class BikeHourData(BaseModel):
+    ''' cette class permet de définir le format des données à envoyer à BikeData '''
     dteday: str
     hr : int
     weathersit : WeathersitEnum
@@ -18,5 +21,5 @@ class BikeHourData(BaseModel):
     temp : float
     atemp : float
 
-
+# On recoit les 24 BikeHourData
 BikeData = List[BikeHourData]
